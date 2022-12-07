c850fd73a928ebcfec8b67679afc3b3193313e1f
Eric-NC <77072522+Eric-NC@users.noreply.github.com>
2022-12-07 08:44:27 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-#![feature(let_chains)]
-
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Op {
+    Num(f64),
+    Neg,
     Add,
     Sub,
     Mul,
     Div,
-    Num(f64),
 }
 
 #[derive(Debug)]
 struct Stack {
-    ops: Vec<Op>,
-    file_index: usize,
     file: String,
+    file_index: usize,
+    ops: Vec<Op>,
+    error: bool,
 }
 
 #[cfg(test)]
@@ -23,12 +23,58 @@ fn it_works() {
 }
 
 fn main() {
-    let text = "5 + 10";
-    let mut stack = Stack::new(text);
+    let mut input = String::new();
+ 
+    std::io::stdin().read_line(&mut input).expect("failed to read input");
+    
+    let mut stack = Stack::new(input);
+
+    if stack.file.trim().is_empty() {
+        eprintln!("ERROR | no input");
+        return;
+    }
 
     parse(&mut stack);
 
-    println!("{stack:?}");
+    if stack.error {
+        return;
+    }
+
+    let result = eval(&mut stack.ops).expect("failed to calculate");
+    let text = &stack.file;
+
+    println!("{text} = {result}")
+}
+
+fn eval(ops: &mut Vec<Op>) -> Option<f64> {
+    let mut numbers = Vec::new();
+
+    for &mut op in ops {
+        let value = eval_op(op, &mut numbers)?;
+
+        numbers.push(value);
+    }
+
+    numbers.pop()
+}
+
+fn eval_op(op: Op, n: &mut Vec<f64>) -> Option<f64> {
+    Some(match op {
+        Op::Num(num) => num,
+        Op::Neg => -n.pop()?,
+        Op::Add => n.pop()? + n.pop()?,
+        Op::Mul => n.pop()? * n.pop()?,
+        Op::Sub => {
+            let right = n.pop()?;
+            let left = n.pop()?;
+            left - right
+        }
+        Op::Div => {
+            let right = n.pop()?;
+            let left = n.pop()?;
+            left / right
+        }
+    })
 }
 
 fn parse(stack: &mut Stack) {
@@ -36,68 +82,96 @@ fn parse(stack: &mut Stack) {
 }
 
 fn parse_add(stack: &mut Stack) {
-    let index = stack.ops.len();
     parse_mul(stack);
-    stack.skip_whitespace();
-    if stack.match_char(|c| c == '+') {
-        stack.ops.insert(index, Op::Add);
+    if stack.match_str("+") {
         parse_mul(stack);
-    } else if stack.match_char(|c| c == '-') {
-        stack.ops.insert(index, Op::Sub);
+        stack.ops.push(Op::Add);
+    } else if stack.match_str("-") {
         parse_mul(stack);
+        stack.ops.push(Op::Sub);
     }
 }
 
 fn parse_mul(stack: &mut Stack) {
-    parse_paren(stack);
+    parse_unary(stack);
+    if stack.match_str("*") {
+        parse_unary(stack);
+        stack.ops.push(Op::Mul);
+    } else if stack.match_str("/") {
+        parse_unary(stack);
+        stack.ops.push(Op::Div);
+    } else if stack.match_str("(") {
+        parse(stack);
+        if !stack.match_str(")") {
+            stack.error(stack.file_index, "expected )");
+        }
+        stack.ops.push(Op::Mul);
+    }
+}
+
+fn parse_unary(stack: &mut Stack) {
+    if stack.match_str("-") {
+        parse_paren(stack);
+        stack.ops.push(Op::Neg);
+    } else {
+        parse_paren(stack);
+    }
 }
 
 fn parse_paren(stack: &mut Stack) {
-    parse_num(stack);
+    if stack.match_str("(") {
+        parse(stack);
+        if !stack.match_str(")") {
+            stack.error(stack.file_index, "expected )");
+        }
+    } else {
+        parse_num(stack);
+    }
 }
 
 fn parse_num(stack: &mut Stack) {
-    if let Ok(num) = stack.next_word().parse() {
-        stack.advance_word();
+    let error_index = stack.file_index;
+    if let Some(num) = stack.read_number() {
         stack.ops.push(Op::Num(num));
+    } else {
+        stack.error(error_index, "expected number");
     }
 }
 
 impl Stack {
-    fn new(file: impl Into<String>) -> Self {
+    fn new(file_contents: impl Into<String>) -> Self {
+        let mut file = file_contents.into();
+        file.retain(|c| !c.is_whitespace());
+
         Self {
-            ops: Vec::new(),
+            file,
             file_index: 0,
-            file: file.into(),
+            ops: Vec::new(),
+            error: false,
         }
     }
 
     fn next(&self) -> &str {
         &self.file[self.file_index..]
     }
-    fn next_char(&self) -> Option<char> {
-        self.next().chars().next()
-    }
-    fn next_word(&mut self) -> &str {
-        self.skip_whitespace();
-        self.next().split_whitespace().next().unwrap_or("")
-    }
-    fn advance_word(&mut self) {
-        self.file_index += self.next_word().len();
-    }
-    fn match_char(&mut self, predicate: impl FnOnce(char) -> bool) -> bool {
-        let mut chars = self.next().char_indices();
-        let next = chars.next();
-        if let Some((_, c)) = next && predicate(c) {
-            if let Some((i, _)) = chars.next() {
-                self.file_index = i;
-            } else {
-                self.file_index = self.file.len();
+    fn read_number(&mut self) -> Option<f64> {
+        let start = self.file_index;
+        let mut length = 0;
+        let mut decimal = false;
+        for c in self.next().chars() {
+            if c == '.' {
+                if !decimal {
+                    decimal = true;
+                } else {
+                    break;
+                }
+            } else if !c.is_digit(10) {
+                break;
             }
-            true
-        } else {
-            false
+            length += 1;
         }
+        self.file_index = start + length;
+        self.file[start..start + length].parse().ok()
     }
     fn match_str(&mut self, string: &'static str) -> bool {
         if self.next().starts_with(string) {
@@ -107,13 +181,14 @@ impl Stack {
             false
         }
     }
-    fn skip_whitespace(&mut self) {
-        while let Some((i, c)) = self.next().char_indices().next() {
-            if c.is_whitespace() {
-                self.file_index += i;
-            } else {
-                break;
-            }
+    fn error(&mut self, pos: usize, message: &'static str) {
+        self.error = true;
+
+        let character = self.file[..pos].trim().chars().last();
+        if let Some(c) = character {
+            eprintln!("ERROR | {message} (just after the {c})")
+        } else {
+            eprintln!("ERROR | {message}")
         }
     }
 }
